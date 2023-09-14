27864bb4b8816fb5f5c102f273e65bb050f7d5f8
josem298207 <144580318+josem298207@users.noreply.github.com>
2023-09-14 18:50:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+print('Welcome to the C2C elite 101 chatbot!')
+name = input('What is your name? ')
+age = input('How old are you? ')
+print(f'Welcome {name}! Oh, to be {age} again. How can I help you today?\n')
+print('Please choose from the following options\n1. Place holder option \n2. Place holder option 2\n3. Place holder option 3\n4. Exit')
+
+answer = input('Enter the number of your choice: ')
+
+if answer == '4':
+  print(f'Goodbye {name}! Have a good day!')
+
 
